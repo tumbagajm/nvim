e18bc64e783070877467453669310991ad05c028
jm <tumbagajohn@gmail.com>
2024-12-09 02:08:04 +0800

    Added ejs to emmet

--- lua/plugins/emmet.lua
+++ lua/plugins/emmet.lua
@@ -1,12 +1,23 @@
--- emmet.lua
-
 return {
-  "mattn/emmet-vim",  -- Emmet plugin
-
-  -- Configuring Emmet plugin
+  "mattn/emmet-vim",
+  ft = { "html", "css", "ejs" }, -- Load only for these file types
   config = function()
-    -- Mapping <Tab> to expand Emmet abbreviation
+    -- Add EJS support
+    vim.g.user_emmet_settings = {
+      filetype = {
+        ejs = { -- Treat .ejs files as HTML
+          extends = "html",
+        },
+      },
+    }
+
+    -- Map <Tab> for Emmet expansion in insert mode
     vim.api.nvim_set_keymap('i', '<Tab>', '<Plug>(emmet-expand-abbr)', { noremap = false, silent = true })
+
+    -- Ensure .ejs files are treated correctly by Emmet
+    vim.cmd([[
+      autocmd BufNewFile,BufRead *.ejs setlocal filetype=ejs
+    ]])
   end,
 }
 
