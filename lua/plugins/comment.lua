3428440c4c322f84dcc5f1bd34e62323bfc7a04f
jm <tumbagajohn@gmail.com>
2024-12-09 20:11:12 +0800

    Updated emmet, and added vim-commentary

---
+++ lua/plugins/comment.lua
@@ -0,0 +1,7 @@
+return {
+  "numToStr/Comment.nvim",
+  config = function()
+    require('comment')  -- This will load the comment.lua file
+  end
+}
+
--- lua/plugins/emmet.lua
+++ lua/plugins/emmet.lua
@@ -2,7 +2,7 @@ return {
   "mattn/emmet-vim",
   ft = { "html", "css", "ejs" }, -- Load only for these file types
   config = function()
-    -- Add EJS support
+    -- Add EJS support by treating .ejs files as HTML
     vim.g.user_emmet_settings = {
       filetype = {
         ejs = { -- Treat .ejs files as HTML
@@ -12,11 +12,16 @@ return {
     }
 
     -- Map <Tab> for Emmet expansion in insert mode
-    vim.api.nvim_set_keymap('i', '<Tab>', '<Plug>(emmet-expand-abbr)', { noremap = false, silent = true })
+    vim.api.nvim_set_keymap('i', '<Tab>', '<Plug>(emmet-expand-abbr)', { noremap = true, silent = true })
 
     -- Ensure .ejs files are treated correctly by Emmet
     vim.cmd([[
-      autocmd BufNewFile,BufRead *.ejs setlocal filetype=ejs
+      autocmd FileType ejs setlocal filetype=html
+    ]])
+
+    -- Disable Emmet in JavaScript/TypeScript files to avoid conflicts
+    vim.cmd([[
+      autocmd FileType javascript,typescript,js,ts let g:user_emmet_mode='none'
     ]])
   end,
 }
