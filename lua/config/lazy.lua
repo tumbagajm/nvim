5bec106ae443bef77eb0eded5c2e95d5f98fbff4
jm <tumbagajohn@gmail.com>
2024-12-09 20:20:47 +0800

    Updated folder structure

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -27,32 +27,9 @@ require("lazy").setup({
     -- import your plugins
     { import = "plugins" },
     { 'neovim/nvim-lspconfig' },
-    { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-    {
-    'windwp/nvim-autopairs',
-    event = "InsertEnter",
-    config = true
-    -- use opts = {} for passing setup options
-    -- this is equivalent to setup({}) function
-},
-    {
-	"nvim-treesitter/nvim-treesitter", 
-	build = ":TSUpdate"},
-	config = function () 
-      	local configs = require("nvim-treesitter.configs")
-
-      	configs.setup({
-        	ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html" },
-          	sync_install = false,
-          	highlight = { enable = true },
-          	indent = { enable = true },  
-        	})
-    	end
-  },
-  -- Configure any other settings here. See the documentation for more details.
-  -- colorscheme that will be used when installing plugins.
-  install = { colorscheme = { "habamax" } },
-  -- automatically check for plugin updates
-  checker = { enabled = true },
+    { "catppuccin/nvim", name = "catppuccin", priority = 1000 }, 
+    install = { colorscheme = { "habamax" } },
+    -- automatically check for plugin updates
+    checker = { enabled = true },
 })
 
