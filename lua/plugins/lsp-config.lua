abb85311ce67da1ce6407e149d649488c7e33074
tumbagajm <tumbagajohn@gmail.com>
2024-12-17 01:02:56 +0800

    Added alpha and autocompletion

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,73 +1,42 @@
 return {
   {
-    "williamboman/mason.nvim", 
+    "williamboman/mason.nvim",
+    lazy = false,
     config = function()
       require("mason").setup()
-    end
+    end,
   },
   {
     "williamboman/mason-lspconfig.nvim",
-    config = function()
-      require("mason-lspconfig").setup({
-        ensure_installed = {
-          "lua_ls",
-          "ts_ls",
-          "tailwindcss"
-        }
-      })
-    end
+    lazy = false,
+    opts = {
+      auto_install = true,
+    },
   },
   {
     "neovim/nvim-lspconfig",
+    lazy = false,
     config = function()
+      local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
       local lspconfig = require("lspconfig")
-      
-      -- Define on_attach and capabilities manually
-      local on_attach = function(client, bufnr)
-        -- Add custom keybindings for LSP-related actions
-        vim.api.nvim_buf_set_keymap(bufnr, 'n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', { noremap = true, silent = true })
-        vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', { noremap = true, silent = true })
-        vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', { noremap = true, silent = true })
-      end
-
-      local capabilities = vim.lsp.protocol.make_client_capabilities()
-
-      lspconfig.lua_ls.setup({})
       lspconfig.ts_ls.setup({
-        on_attach = on_attach,
-        capabilities = capabilities,
+        capabilities = capabilities
       })
-      lspconfig.tailwindcss.setup({
-        on_attach = on_attach,
-        capabilities = capabilities,
+      lspconfig.solargraph.setup({
+        capabilities = capabilities
       })
-    end
-  },
-  {
-    "hrsh7th/nvim-cmp",
-    config = function()
-      local cmp = require("cmp")
-      cmp.setup({
-        snippet = {
-          expand = function(args)
-            vim.fn["vsnip#anonymous"](args.body)  -- Adjust if you're using a different snippet engine
-          end,
-        },
-        mapping = {
-          ['<C-p>'] = cmp.mapping.select_prev_item(),
-          ['<C-n>'] = cmp.mapping.select_next_item(),
-          ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-        },
-        sources = {
-          { name = 'nvim_lsp' },
-          { name = 'vsnip' },  -- Adjust if you're using a different snippet engine
-          { name = 'buffer' },
-        },
+      lspconfig.html.setup({
+        capabilities = capabilities
       })
-    end
-  },
-  {
-    "hrsh7th/vim-vsnip",
-  }
-}
+      lspconfig.lua_ls.setup({
+        capabilities = capabilities
+      })
 
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+      vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+    end,
+  },
+}
