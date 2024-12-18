403061fa1f5323b4bb5cd28a8db0e7a772a8b2bf
jm <tumbagajohn@gmail.com>
2024-12-18 13:57:19 +0800

    Updated keymaps for cmp

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -1,6 +1,6 @@
 return {
   {
-    "hrsh7th/cmp-nvim-lsp"
+    "hrsh7th/cmp-nvim-lsp",
   },
   {
     "L3MON4D3/LuaSnip",
@@ -31,6 +31,17 @@ return {
           ["<C-Space>"] = cmp.mapping.complete(),
           ["<C-e>"] = cmp.mapping.abort(),
           ["<CR>"] = cmp.mapping.confirm({ select = true }),
+          ["<C-n>"] = cmp.mapping(function()
+            if require("luasnip").jumpable(1) then
+              require("luasnip").jump(1) -- Jump forward in the snippet
+            end
+          end, { "i", "s" }),
+
+          ["<C-p>"] = cmp.mapping(function()
+            if require("luasnip").jumpable(-1) then
+              require("luasnip").jump(-1) -- Jump backward in the snippet
+            end
+          end, { "i", "s" }),
         }),
         sources = cmp.config.sources({
           { name = "nvim_lsp" },
