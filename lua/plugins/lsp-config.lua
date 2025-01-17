0ff40920423cd2714bf2cb9d9e63ab8c272debb5
jm <tumbagajohn@gmail.com>
2025-01-17 13:44:29 +0800

    Added Alt + Enter for actions

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -13,7 +13,7 @@ return {
       ensure_installed = {
         "jdtls",
         "ts_ls",
-        "ruby_lsp"
+        "ruby_lsp",
       },
       auto_install = true,
     },
@@ -22,32 +22,38 @@ return {
     "neovim/nvim-lspconfig",
     lazy = false,
     config = function()
-      local capabilities = require('cmp_nvim_lsp').default_capabilities()
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
       local lspconfig = require("lspconfig")
       lspconfig.ruby_lsp.setup({
-        capabilities = capabilities
+        capabilities = capabilities,
       })
       lspconfig.ts_ls.setup({
-        capabilities = capabilities
+        capabilities = capabilities,
       })
       lspconfig.solargraph.setup({
-        capabilities = capabilities
+        capabilities = capabilities,
       })
       lspconfig.html.setup({
-        capabilities = capabilities
+        capabilities = capabilities,
       })
       lspconfig.lua_ls.setup({
-        capabilities = capabilities
+        capabilities = capabilities,
       })
       lspconfig.jdtls.setup({
-        capabilities = capabilities
+        capabilities = capabilities,
+        cmd = { "jdtls" }, -- Path to the Java language server executable
+        root_dir = lspconfig.util.root_pattern(".git", "pom.xml", "build.gradle"),
       })
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
       vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
       vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
       vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
       vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, {})
+      vim.keymap.set("n", "<A-Enter>", vim.lsp.buf.code_action, { desc = "Code Action (Auto-import)" })
+      vim.keymap.set("n", "<A-Insert>", function()
+        require("jdtls").code_action() -- Trigger the jdtls code action menu
+      end, { desc = "Generate Code (Getters/Setters)" })
     end,
   },
 }
