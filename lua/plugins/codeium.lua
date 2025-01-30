39e0f21b52e98e24813801ddba233718d2a33e16
jm <tumbagajohn@gmail.com>
2025-01-30 16:35:35 +0800

    updated codeium keymap

--- lua/plugins/codeium.lua
+++ lua/plugins/codeium.lua
@@ -6,6 +6,7 @@ return {
 
 		-- Keybindings for Codeium
     vim.api.nvim_set_keymap("i", "<Tab>", "v:lua.codeium_fallback()", { expr = true, noremap = true, silent = true })
+
 		vim.api.nvim_set_keymap("i", "<C-n>", "<Plug>(codeium-next)", {})
 		vim.api.nvim_set_keymap("i", "<C-p>", "<Plug>(codeium-prev)", {})
 	end,
