4c5feacc01eeba7b8d170ea80eec3d3d93d72f39
jm <tumbagajm@gmail.com>
2025-04-24 17:56:49 +0800

    Updated codeium - codium to windsurf

--- lua/plugins/codeium.lua
+++ lua/plugins/codeium.lua
@@ -1,13 +1,8 @@
 return {
-	"Exafunction/codeium.vim",
-	config = function()
-		-- Enable Codeium globally (optional)
-		vim.g.codeium_enabled = true
-
-		-- Keybindings for Codeium
-    vim.api.nvim_set_keymap("i", "<Tab>", "v:lua.codeium_fallback()", { expr = true, noremap = true, silent = true })
-
-		vim.api.nvim_set_keymap("i", "<C-n>", "<Plug>(codeium-next)", {})
-		vim.api.nvim_set_keymap("i", "<C-p>", "<Plug>(codeium-prev)", {})
-	end,
+  'Exafunction/windsurf.vim',
+  event = 'BufEnter',
+  config = function()
+    vim.keymap.set('n', '<leader>ws', function() require('windsurf').save_layout() end, { desc = "Save window layout" })
+    vim.keymap.set('n', '<leader>wl', function() require('windsurf').load_layout() end, { desc = "Load window layout" })
+  end
 }
