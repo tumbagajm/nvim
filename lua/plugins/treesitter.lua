a18602277e9c5539adcecdbb69f12b4326516162
asianlink-jm <jm.tumbaga@asianlink.ai>
2025-07-10 12:32:06 +0800

    Added php support

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,25 +1,35 @@
 return {
-	{
-		"nvim-treesitter/nvim-treesitter",
-		build = ":TSUpdate",
-		config = function()
-			local configs = require("nvim-treesitter.configs")
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+      local configs = require("nvim-treesitter.configs")
 
-			configs.setup({
-				-- ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript" },
-				auto_install = true,
-				sync_install = false,
-				highlight = { enable = true },
-				indent = { enable = true },
-				fold = { enable = true },
-			})
-		end,
-	},
-	{
-		"windwp/nvim-ts-autotag",
-		event = "InsertEnter",
-		config = function()
-			require("nvim-ts-autotag").setup()
-		end,
-	},
+      configs.setup({
+        -- ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript" },
+        ensure_installed = {
+          "php",        -- Laravel backend
+          "blade",      -- Blade templates
+          "html",       -- HTML structure
+          "css",        -- CSS for jQuery/React
+          "javascript", -- jQuery
+          "tsx",        -- React (JSX/TSX)
+          "lua",        -- For Neovim config
+          "json",       -- Often used with APIs/configs
+        },
+        auto_install = true,
+        sync_install = false,
+        highlight = { enable = true },
+        indent = { enable = true },
+        fold = { enable = true },
+      })
+    end,
+  },
+  {
+    "windwp/nvim-ts-autotag",
+    event = "InsertEnter",
+    config = function()
+      require("nvim-ts-autotag").setup()
+    end,
+  },
 }
